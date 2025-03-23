f530aa316192390269e61c12654055433ed7c417
sylee999 <sylee999@gmail.com>
2025-03-23 23:43:25 +0900

    add padding in Main component to show the end of contents

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -5,7 +5,7 @@ interface MainProps {
 }
 
 const Main: React.FC<MainProps> = ({ children }) => {
-  return <div className="flex flex-1 flex-col gap-4 p-4">{children}</div>
+  return <div className="flex flex-1 flex-col gap-4 p-4 pb-16">{children}</div>
 }
 
 export default Main
